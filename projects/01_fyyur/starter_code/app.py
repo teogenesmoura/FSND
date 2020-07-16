d3e7d1f8c9b07926bf3fea17fd635c5bd685cfa9
Teogenes Moura <teogenesmoura@Teogeness-MacBook-Pro.local>
2020-07-16 16:30:49 -0300

    implemented search for venues

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -104,17 +104,17 @@ def venues():
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for Hop should return "The Musical Hop".
-  # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
-  response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }
+  response['data'] = []
+  search_term = request.form.get('search_term').lower()
+  venues = Venue.query.filter(func.lower(Venue.name).contains(search_term))
+  count = 0
+  for venue in venues:
+    count += 1 
+    data = {}
+    data['id'] = venue.id 
+    data['name'] = venue.name
+    response['data'].append(data)
+  response['count'] = count  
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/venues/<int:venue_id>')
@@ -211,7 +211,6 @@ def search_artists():
     data = {}
     data['id'] = artist.id 
     data['name'] = artist.name 
-    data['num_upcoming_shows'] = len(get_upcoming_shows_for_artist(artist.id))
     response['data'].append(data)
   response['count'] = count
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
